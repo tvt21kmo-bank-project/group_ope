cff801dfd0c3e0a7561ad1b861afd2f9c94204b9
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2021-11-01 19:05:06 +0200

    esimerkit valmiit

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/toka',function(request, response){
+  console.log('toka');
+  response.send('toka');
+});
+
+router.use(function(request, response, next){
+  console.log("Olen Middleware 1");
+  next();
+});
+
+router.get('/kolmas/:name?',function(request, response){
+  console.log(request.params.name);
+  response.send("Terve "+request.params.name);
+});
+
+router.get('/neljas/:fname/:lname?',function(request,response){
+  console.log("Terve "+request.params.fname+" "+request.params.lname);
+  response.send("Terve "+request.params.fname+" "+request.params.lname);
+});
+
+router.post('/', function(request, response){
+  console.log(request.body);
+  response.send("Terve "+request.body.fname+" "+request.body.lname);
+});
+
+router.put('/:id',function(request, response){
+  response.send("henkilölle "+request.params.id+" Muutetaan nimeksi"+request.body.fname);
+});
+
 module.exports = router;
